fdd212349e4e7f3ef7da29fad3be439a19cd981c
jur0slaw <jerjurko@gmail.com>
2017-05-25 18:05:24 +0200

    Add project files.

---
+++ TCP Serwer/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TCP_Serwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Adress = new System.Windows.Forms.TextBox();
+            this.info_feed = new System.Windows.Forms.ListBox();
+            this.port = new System.Windows.Forms.NumericUpDown();
+            this.stopbutton = new System.Windows.Forms.Button();
+            this.startbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.port)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Adress
+            // 
+            this.Adress.Location = new System.Drawing.Point(53, 38);
+            this.Adress.Name = "Adress";
+            this.Adress.Size = new System.Drawing.Size(268, 20);
+            this.Adress.TabIndex = 0;
+            this.Adress.Text = "Adress";
+            // 
+            // info_feed
+            // 
+            this.info_feed.FormattingEnabled = true;
+            this.info_feed.Location = new System.Drawing.Point(53, 91);
+            this.info_feed.Name = "info_feed";
+            this.info_feed.Size = new System.Drawing.Size(616, 290);
+            this.info_feed.TabIndex = 1;
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(422, 39);
+            this.port.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(247, 20);
+            this.port.TabIndex = 2;
+            // 
+            // stopbutton
+            // 
+            this.stopbutton.Location = new System.Drawing.Point(422, 401);
+            this.stopbutton.Name = "stopbutton";
+            this.stopbutton.Size = new System.Drawing.Size(75, 23);
+            this.stopbutton.TabIndex = 3;
+            this.stopbutton.Text = "Stop";
+            this.stopbutton.UseVisualStyleBackColor = true;
+            this.stopbutton.Click += new System.EventHandler(this.stopbutton_Click);
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(246, 401);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 4;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 447);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.stopbutton);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.info_feed);
+            this.Controls.Add(this.Adress);
+            this.Name = "Form1";
+            this.Text = "Serwer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.port)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Adress;
+        private System.Windows.Forms.ListBox info_feed;
+        private System.Windows.Forms.NumericUpDown port;
+        private System.Windows.Forms.Button stopbutton;
+        private System.Windows.Forms.Button startbutton;
+    }
+}
+
---
+++ TCP Serwer/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+namespace TCP_Serwer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopbutton.Enabled = false;
+        }
+
+        private TcpListener server = null;
+        private TcpClient client = null;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int p = Convert.ToUInt16(port.Value);
+            IPAddress ip = null;
+
+            try
+            {
+                ip = IPAddress.Parse(Adress.Text); 
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Wrong IP adress format.", " Error ");
+                Adress.Text = string.Empty;
+                return;
+            }
+
+            try
+            {
+                server = new TcpListener(ip, p);
+                server.Start();
+
+                client = server.AcceptTcpClient();
+
+                info_feed.Items.Add("Connection established.");
+
+                IPEndPoint srv  = (IPEndPoint)client.Client.RemoteEndPoint;
+
+                info_feed.Items.Add("[" + srv.ToString() + "] : Connection established");
+
+                startbutton.Enabled = false;
+                stopbutton.Enabled = true;
+
+                client.Close();
+                server.Stop();
+            }
+            catch(Exception ex)
+            {
+                info_feed.Items.Add("Error establishing a server.");
+                MessageBox.Show(ex.ToString());
+            }
+
+            
+        }
+
+        private void stopbutton_Click(object sender, EventArgs e)
+        {
+            startbutton.Enabled = true;
+            stopbutton.Enabled = false;
+            MessageBox.Show("Connection closed");
+
+            server.Stop();
+            client.Close();
+        }
+    }
+}
